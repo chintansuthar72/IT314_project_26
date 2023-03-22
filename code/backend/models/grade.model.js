0de6e7f4ba5e356221dc3ac289a6d76f5c64649a
Priyanshu009 <priyanshu.parmar02@gmail.com>
2023-03-22 14:27:23 +0530

    course, file and grade model added

---
+++ code/backend/models/grade.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const gradeSchema = new mongoose.Schema({
+    submission: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Submission',
+        required: true,
+    },
+    student: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    grade: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 100,
+    },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment',
+    }],
+    // files: [{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'File',
+    // }],
+    // folders: [{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Folder',
+    // }],
+    // notifications: [{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Notification',
+    // }],
+}, {
+    timestamps: true,
+});
+
+const Grade = mongoose.model('Grade', gradeSchema);
+
+module.exports = Grade;
