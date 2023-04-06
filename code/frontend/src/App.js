2e7a09da0f9a7e1286a7d7d13bb32570272ba12e
Chintan Suthar <75067919+chintansuthar72@users.noreply.github.com>
2023-04-06 17:44:04 +0000

    Server configuration

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 // import './App.css'
 import SignIn from './signin'
 import SignUp from './signup'
+import Navbar from './Navbar';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
 
   return (
     <div className='App'>
+      <Navbar />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<SignIn />} />
+          <Route path="/signin" element={<SignIn />} />
           <Route path="/SignUp" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
