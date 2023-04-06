dc417ecdc10436a26078804d4c4c12ef8aa69f15
Chintan Suthar <75067919+chintansuthar72@users.noreply.github.com>
2023-04-06 17:56:54 +0000

    User controllers

---
+++ code/backend/controllers/user.controller.js
@@ -0,0 +1,100 @@
+const {User} = require('../models/index.model');
+const response = require('../utils/responses.util');
+const {userValidation} = require('../validations/index.validation');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const loginUser = async (req, res) => {
+    try {
+        // check is user exists
+        const user = await User.findOne({email: req.body.email});
+        if(!user) return response.badRequestResponse(res, 'Email does not exist');
+        
+        // check if password is correct
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if(!validPassword) return response.badRequestResponse(res, 'Invalid password');
+        
+        // create and assign a token
+        const token = jwt.sign({id: user._id, email: user.email}, process.env.SECRET, {expiresIn: '1h'});
+
+        // res.header('authorization', token);
+        res.set('authorization', token);
+        return response.successResponse(res, { token,user});
+
+    } catch (err) {
+        return response.serverErrorResponse(res, err);
+    }
+}
+
+const getAllUsers = async (req, res) => {
+    try {
+        if(!['ADMIN'].includes(req.role))
+            return response.unauthorizedResponse(res);
+        const users = await User.find();
+        return response.successResponse(res, users);
+    } catch (err) {
+        return response.serverErrorResponse(res, err);
+    }
+}
+
+const getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        return response.successResponse(res, user);
+    } catch (err) {
+        return response.serverErrorResponse(res, err);
+    }
+}
+
+const addUser = async (req, res) => {
+    try {
+        const {error} = userValidation.validate(req.body);
+        if(error) return response.badRequestResponse(res, error.details[0].message);
+
+        const user = await User.findOne({email: req.body.email});
+        if(user) return response.badRequestResponse(res, 'Email already exists');
+
+        const username = await User.findOne({username: req.body.username});
+        if(username) return response.badRequestResponse(res, 'Username already exists');
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        req.body.password = hashedPassword;
+
+        const newUser = await User.create(req.body);
+        return response.successfullyCreatedResponse(res, newUser);
+    } catch (err) {
+        return response.serverErrorResponse(res, err);
+    }
+}
+
+const deleteUserById = async (req, res) => {
+    try {
+        if(!['ADMIN'].includes(req.role))
+            return response.unauthorizedResponse(res);
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+        return response.successResponse(res, deletedUser);
+    } catch (err) {
+        return response.serverErrorResponse(res, err);
+    }
+}
+
+const updateUserById = async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.id,{
+            username: req.body.username,
+            phone: req.body.phone,
+        });
+        return response.successResponse(res, updatedUser);
+    } catch (err) {
+        return response.serverErrorResponse(res, err);
+    }
+}
+
+module.exports = {
+    loginUser,
+    getAllUsers,
+    getUserById,
+    addUser,
+    deleteUserById,
+    updateUserById,
+}
