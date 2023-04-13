0905b28cc5c1c0c13d73ad92bba0b1272a68fc29
chintansuthar72 <mrchintansuthar@gmail.com>
2023-04-13 17:12:07 +0530

    Reveal Controller

--- code/backend/controllers/index.controller.js
+++ code/backend/controllers/index.controller.js
@@ -1,10 +1,10 @@
 // import controllers
 const userController = require('./user.controller');
-// const courseController = require('./course.controller');
-// const assignmentController = require('./assignment.controller');
-// const submissionController = require('./submission.controller');
+const courseController = require('./course.controller');
+const assignmentController = require('./assignment.controller');
+const submissionController = require('./submission.controller');
 const commentController = require('./comment.controller');
-// const gradeController = require('./grade.controller');
+const gradeController = require('./grade.controller');
 const announcementController = require('./announcement.controller');
 const messageController = require('./message.controller');
 // const notificationController = require('./notification.controller');
@@ -14,11 +14,11 @@ const messageController = require('./message.controller');
 // export controllers
 module.exports = {
     userController,
-    // courseController,
-    // assignmentController,
-    // submissionController,
+    courseController,
+    assignmentController,
+    submissionController,
     commentController,
-    // gradeController,
+    gradeController,
     announcementController,
     messageController,
     // notificationController,
