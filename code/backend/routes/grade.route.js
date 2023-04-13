2f2413af792f2d5377c60539461f66163c2d9ef9
chintansuthar72 <mrchintansuthar@gmail.com>
2023-04-13 17:11:08 +0530

    Added verification in grade routes

--- code/backend/routes/grade.route.js
+++ code/backend/routes/grade.route.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const Grade = require('../models/grade.model');
+const {verifyToken} = require('../middlewares/authJwt.middleware');
+const {gradeController} = require('../controllers/index.controller');
 
 // update grade
-router.put('/update/:id',updateGrade);
+router.put('/update/:id', [verifyToken], gradeController.updateGrade);
 
 // get grade by submission id
-router.get('/submission/:id',getGradeBySubmission);
+router.get('/submission/:id',[verifyToken], gradeController.getGradeBySubmission);
 
 // get grade by student id
-router.get('/student/:id',getGradeByStudent);
+router.get('/student/:id',[verifyToken], gradeController.getGradeByStudent);
