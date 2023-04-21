eaac4766c4d574a4d2117704d546e24e286ac396
Pranav <pranavthumar01mar@gmail.com>
2023-04-22 01:41:45 +0530

    Added feedback students

---
+++ code/frontend/src/Student_DashBoard_components/Dashboard.js
@@ -0,0 +1,393 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiDrawer from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+// import { mainListItems, secondaryListItems } from './listItems';
+import Chart from './Chart';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PeopleIcon from '@mui/icons-material/People';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import LayersIcon from '@mui/icons-material/Layers';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import LogoutIcon from '@mui/icons-material/Logout';
+import Deposits from './Deposits';
+import Orders from './Orders';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Title from './Title';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { Button } from '@mui/material';
+import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
+
+const set = (keyName, keyValue, ttl) => {
+  const data = {
+      value: keyValue,                  // store the value within this object
+      ttl: Date.now() + (ttl * 1000),   // store the TTL (time to live)
+  }
+  localStorage.setItem(keyName, JSON.stringify(data));
+};
+const get = (keyName) => {
+  const data = localStorage.getItem(keyName);
+  if (!data) {     // if no value exists associated with the key, return null
+      return null;
+  }
+  const item = JSON.parse(data);
+  if (Date.now() > item.ttl) {
+      localStorage.removeItem(keyName);
+      localStorage.removeItem('role');
+      localStorage.removeItem('user');
+      return null;
+  }
+  return item.value;
+};
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    '& .MuiDrawer-paper': {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      boxSizing: 'border-box',
+      ...(!open && {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+          width: theme.spacing(9),
+        },
+      }),
+    },
+  }),
+);
+
+const mdTheme = createTheme();
+
+function DashboardContent({setIsLoggedIn,navigate,user }) {
+  const [open, setOpen] = React.useState(false);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
+  const [error, setError] = useState(null);
+  const [rows, setRows] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/user/courses',{headers:{'Authorization': get('token')}})
+      .then((resp)=>{   // if no error
+        console.log(resp);
+        setRows(resp.data.data);
+      })
+      .catch((err)=>{
+        console.log(err);
+        setError(err.response.data.error);
+      })
+  },[]);
+
+  return (
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="absolute" open={open}>
+          <Toolbar
+            sx={{
+              pr: '24px', // keep right padding when drawer closed
+            }}
+          >
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: '36px',
+                ...(open && { display: 'none' }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              Dashboard
+            </Typography>
+            {/* <IconButton color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton> */}
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+              px: [1],
+            }}
+          >
+            <IconButton onClick={toggleDrawer}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List component="nav">
+            {/* {mainListItems}*/}
+            <ListItemButton onClick={() => {
+              navigate('/dashboard', {
+                state: {
+                  user : user
+                }
+              });
+            }}>
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItemButton>
+            {/* <ListItemButton>
+              <ListItemIcon>
+                <ShoppingCartIcon />
+              </ListItemIcon>
+              <ListItemText primary="My Profile" />
+            </ListItemButton> */}
+            <ListItemButton onClick={() => {
+              navigate('/profile', {
+                state: {
+                  user : user,
+                }
+              });
+            }}>
+              <ListItemIcon>
+                <PeopleIcon />
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+            <ListItemButton onClick={() => {
+              navigate('/create', {
+                state: {
+                  user : user,
+                }
+              });
+            }}>
+              <ListItemIcon>
+                <AddCircleOutlineIcon/>
+              </ListItemIcon>
+              <ListItemText primary="New Course" />
+            </ListItemButton>
+            <ListItemButton onClick={() => {
+              navigate('/progress', {
+                state: {
+                  user : user,
+                }
+              });
+            }}>
+              <ListItemIcon>
+                <BarChartIcon />
+              </ListItemIcon>
+              <ListItemText primary="Your Progress" />
+            </ListItemButton>
+            <ListItemButton onClick={() => {
+              navigate('/feedback', {
+                state: {
+                  user : user,
+                }
+              });
+            }}>
+              <ListItemIcon>
+                <LogoutIcon />
+              </ListItemIcon>
+              <ListItemText primary="Feedback Form" />
+            </ListItemButton>
+          </List>
+            <ListItemButton onClick={() => {
+              localStorage.removeItem('token');
+              localStorage.removeItem('user');
+              localStorage.removeItem('role');
+              setIsLoggedIn(false);
+            }}>
+              <ListItemIcon>
+                <LogoutIcon />
+              </ListItemIcon>
+              <ListItemText primary="Log out" />
+            </ListItemButton>
+            
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Grid container spacing={3}>
+              {/* Chart */}
+              {/* <Grid item xs={12} md={8} lg={9}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 240,
+                  }}
+                >
+                  <Chart />
+                </Paper>
+              </Grid> */}
+              {/* Recent Deposits */}
+              {/* <Grid item xs={12} md={4} lg={3}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 240,
+                  }}
+                >
+                  <Deposits />
+                </Paper>
+              </Grid> */}
+              {/* Recent Orders */}
+              <Grid item xs={12}>
+                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                  {/* <Orders /> */}
+                  {error && <div>{error}</div>}
+                  <>
+                    <Title>Classes</Title>
+                    <Table size="medium">
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Course Code</TableCell>
+                          <TableCell>Name</TableCell>
+                          <TableCell>Instructor</TableCell>
+                          {/* <TableCell>Class link</TableCell>
+                          <TableCell align="right">number of students</TableCell> */}
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {rows.map((row) => (
+                          <TableRow key={row.course._id}>
+                            <TableCell>{row.course.courseCode}</TableCell>
+                            <TableCell>{row.course.name}</TableCell>
+                            <TableCell>{row.instructor}</TableCell>
+                            <TableCell align='right'><Button onClick={() => {
+                              navigate('/manage',{
+                                state: {
+                                  course: row.course,
+                                  instructor: row.instructor, 
+                                  user: user,
+                                }
+                              })
+                            }}>Open</Button></TableCell>
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                    {/* <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+                      See more orders
+                    </Link> */}
+                  </>
+                </Paper>
+              </Grid>
+            </Grid>
+            {/* <Copyright sx={{ pt: 4 }} /> */}
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default function Dashboard() {
+  const navigate = useNavigate();
+  const {state} = useLocation();
+  const [isLoggedIn, setIsLoggedIn] = React.useState(get('token') !== null);
+  React.useEffect(() => {
+    if(get('token') === null ){
+      navigate('/');
+    }
+    if( state === null) {
+      localStorage.removeItem('token');
+      navigate('/');
+    }
+  },[isLoggedIn]);
+  return <DashboardContent  setIsLoggedIn={setIsLoggedIn} navigate={navigate} user={state.user}/>;
+}
---
+++ code/frontend/src/Student_DashBoard_components/Feedback.js
@@ -0,0 +1,97 @@
+import React, {useState} from 'react';
+import { format } from 'date-fns'
+import './Feedback.css';  // import the CSS file
+
+format(new Date(), 'dd.MM.yyyy')
+
+function Feedback() {
+  const [studentName, setStudentName] = useState('');
+  const [studentID, setStudentID] = useState('');
+  const [startLeaveDate, setStartLeaveDate] = useState('');
+  const [endLeaveDate, setEndLeaveDate] = useState('');
+  const [reason, setReason] = useState('');
+  const [phoneNo, setPhoneNo] = useState('');
+  const [email, setEmail] = useState('');
+  
+  const studentNameHandler = (event) => {
+    setStudentName(event.target.value);
+  }
+
+  const studentIDHandler = (event) => {
+    setStudentID(event.target.value);
+  }
+
+  const startLeaveDateHandler = (event) => {
+    setStartLeaveDate(event.target.value);
+  }
+
+  const endLeaveDateHandler = (event) => {
+    setEndLeaveDate(event.target.value);
+  }
+
+  const reasonHandler = (event) => {
+    setReason(event.target.value);
+  }
+
+  const phoneNoHandler = (event) => {
+    setPhoneNo(event.target.value);
+  }
+
+  const emailHandler = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(studentName);
+    setStudentName('');
+    console.log(studentID);
+    setStudentID('');
+    console.log(startLeaveDate);
+    setStartLeaveDate('');
+    console.log(endLeaveDate);
+    setEndLeaveDate('');
+    console.log(reason);
+    setReason('');
+    console.log(phoneNo);
+    setPhoneNo('');
+    console.log(email);
+    setEmail('');
+  }
+
+  return(
+    <form className="form" onSubmit={onSubmitHandler} style={{marginTop:'30px'}}>
+      <h1 style={{textAlign: 'center'}}>FEEDBACK FORM</h1><br/><br/><br/>
+      <div className="form-group" style={{display: 'flex'}}>
+        <label className="label">Student Name:</label>
+        <input className="input" id="student-name" type="text" value={studentName} onChange={studentNameHandler} required  />
+      </div>
+      <div className="form-group"  style={{display: 'flex'}}>
+        <label className="label">Student ID:</label>
+        <input className="input" id="student-id" type="text" value={studentID} onChange={studentIDHandler} required />
+      </div>
+      <div className="form-group"  style={{display: 'flex'}}>
+        <label className="label">Submit Date:</label>
+        <input className="input" id="date1" type="date" value={startLeaveDate} onChange={startLeaveDateHandler} required/>
+       
+      </div>
+      <div className="form-group"  style={{display: 'flex'}}>
+        <label className="label">Feedback:</label>
+        <textarea className="textarea" id="reason" value={reason} onChange={reasonHandler} required/>
+      </div>
+      <div className="form-group"  style={{display: 'flex'}}>
+        <label className="label">Email ID: </label>
+        <input className="input" id="email" type="email" value={email} onChange={emailHandler} required />
+      </div>
+      <div className="form-group"  style={{display: 'flex'}}>
+        <label className="label">Phone:</label>
+        <input className="input" id="phone" type="tel" value={phoneNo} onChange={phoneNoHandler} required />
+      </div>
+
+      <button  className="button" type="submit">Submit</button>
+    </form>
+
+  )
+}
+
+export default Feedback;
