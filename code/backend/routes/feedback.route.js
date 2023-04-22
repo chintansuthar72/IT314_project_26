d5ba5a924bf0085b1c22f7dd77c83f0814343e2d
NikunjAdhiya <202001277@daiict.ac.in>
2023-04-22 22:30:24 +0530

    feedback route

--- code/backend/routes/feedback.route.js
+++ code/backend/routes/feedback.route.js
@@ -5,5 +5,9 @@ const { verifyToken } = require('../middlewares/authJwt.middleware');
 // POST new feedback
 router.post('/course/:id', [verifyToken], feedbackcontroller.addFeedback);
 
+// GET all feedbacks by course id
+router.get('/course/:id', [verifyToken], feedbackcontroller.getAllFeedbacksByCourseId);
+
+
 
 module.exports = router;
