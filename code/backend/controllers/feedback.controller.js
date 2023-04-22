cadb974a84aab2c9c744b47e43a0736c2c1605c4
NikunjAdhiya <202001277@daiict.ac.in>
2023-04-22 22:30:37 +0530

    feedback controller

--- code/backend/controllers/feedback.controller.js
+++ code/backend/controllers/feedback.controller.js
@@ -7,9 +7,6 @@ exports.addFeedback = async (req, res) => {
     try {
         if (!['STUDENT'].includes(req.role))
             return response.unauthorizedResponse(res);
-        req.body.course = req.params.id;
-        const feedback_data = req.body.Feedback;
-        // const feedback = await Feedback.create(req.body);
 
          const course = await Course.findById(req.params.id);
         if (!course)
@@ -17,12 +14,10 @@ exports.addFeedback = async (req, res) => {
 
             const feedback = await Feedback.create
             ({
-                student_name: req.body.student_name,
-                student_id: req.body.student_id,
-                submit_date: req.body.submit_data,
-                feedback: req.body.feedback,
-                email_id: req.body.email_id,
-                phone: req.body.phone,
+                topic: req.body.topic,
+                description: req.body.description,
+                course: req.params.id,
+
                 
             });
 
@@ -31,6 +26,21 @@ exports.addFeedback = async (req, res) => {
         }
       
     catch (err) {
+        return response.serverErrorResponse(res, err);
+    }
+}
+
+// GET all feedbacks by course id
+exports.getAllFeedbacksByCourseId = async (req, res) => {
+    try {
+        if (!['ADMIN','TEACHER'].includes(req.role))
+            return response.unauthorizedResponse(res);
+        const course = await Course.findById(req.params.id);
+        if (!course)
+            return response.notFoundResponse(res, 'Course not found');
+        const feedbacks = await Feedback.find({ course: req.params.id });
+        return response.successResponse(res, feedbacks);
+    } catch (err) {
         return response.serverErrorResponse(res, err);
     }
 }
