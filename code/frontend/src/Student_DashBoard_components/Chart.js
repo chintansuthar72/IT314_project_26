be302675bf6e68d8c1f5446389df673c60d18abf
Priyanshu009 <priyanshu.parmar02@gmail.com>
2023-04-23 01:05:39 +0530

    axios added in progress chart

--- code/frontend/src/Student_DashBoard_components/Chart.js
+++ code/frontend/src/Student_DashBoard_components/Chart.js
@@ -2,33 +2,49 @@ import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
 import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
 import Title from './Title';
+import axios from 'axios';
+import { useState,useEffect } from 'react';
+import {  CartesianGrid, Tooltip, Legend } from 'recharts';
 
 // Generate Sales Data
 function createData(time, amount) {
   return { time, amount };
 }
 
-const data = [
-  createData('00:00', 0),
-  createData('03:00', 300),
-  createData('06:00', 600),
-  createData('09:00', 800),
-  createData('12:00', 1500),
-  createData('15:00', 2000),
-  createData('18:00', 2400),
-  createData('21:00', 2400),
-  createData('24:00', undefined),
-];
+// const data = [
+//   createData('00:00', 0),
+//   createData('03:00', 300),
+//   createData('06:00', 600),
+//   createData('09:00', 800),
+//   createData('12:00', 1500),
+//   createData('15:00', 2000),
+//   createData('18:00', 2400),
+//   createData('21:00', 2400),
+//   createData('24:00', undefined),
+// ];
 
 export default function Chart() {
   const theme = useTheme();
+
+  const [usedata, setData] = React.useState([]);
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/albums")
+    .then((res) => {
+      console.log(res.data);
+      let modData = [];
+      res.data.forEach((item) => {
+        modData.push(createData(item.title, item.userId));
+      });
+      setData(modData);
+    })
+  }, []);
 
   return (
     <React.Fragment>
       <Title>Progress</Title>
       <ResponsiveContainer>
         <LineChart
-          data={data}
+          data={usedata}
           margin={{
             top: 16,
             right: 16,
@@ -54,7 +70,7 @@ export default function Chart() {
                 ...theme.typography.body1,
               }}
             >
-              Grades (%)
+              Marks (out of 100)
             </Label>
           </YAxis>
           <Line
